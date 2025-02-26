6787be1692b51ef4cab1d36ce51afa68b9d2d42f
atul <atul.etech2011@gmail.com>
2025-02-26 19:26:36 +0530

    change config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"], // Allows SVGs to be imported as React components
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
